b2e3887a0ba55238cb76d9285333ffb3eac55c66
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-06-01 22:26:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -460,9 +460,9 @@ if ativo:
     #Stop Atr
     else:
             #Cálculo do indicador Open-Atr
-            high_low = df['high'] - df['low']
-            high_close = np.abs(df['high'] - df['close'].shift())
-            low_close = np.abs(df['low'] - df['close'].shift())
+            high_low = df['High'] - df['Low']
+            high_close = np.abs(df['High'] - df['Close'].shift())
+            low_close = np.abs(df['Low'] - df['Close'].shift())
             ranges = pd.concat([high_low, high_close, low_close], axis=1)
             true_range = np.max(ranges, axis=1)
             df['atr'] = (true_range.rolling(10).sum())/10.00
