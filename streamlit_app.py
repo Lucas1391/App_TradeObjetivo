3d76d4e530fcb8d4389561db8c1d657457a2fe15
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-06-01 22:16:49 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -470,13 +470,14 @@ if ativo:
             df['Stop_Atr_Upper'] = df['HL'] + 3*df['atr']
             df['Stop_Atr_Donw'] = df['HL'] - 3*df['atr']
             def SuperTrend(df):
+                SuperTrend = []
                 for i in range(1,df):
                     if df['Close'][i] < df['Stop_Atr_Upper'][i]:
-                        df['SuperTrend'][i] = df['Stop_Atr_Upper'][i]
+                        SuperTrend.append(df['Stop_Atr_Upper'][i])
                     elif df['Stop_Atr_Upper'][i] < df['Close'][i]:
-                        df['SuperTrend'][i] = df['Stop_Atr_Donw']
-                 return df['SuperTrend']
-            df = SuperTrend(df)
+                        SuperTrend.append(df['Stop_Atr_Donw'][i])
+                 return  SuperTrend
+            df['SuperTrend'] = SuperTrend(df)
             def podeComprar(i,dados):
                 if (dados['Close'][i-1] < df['Stop_Atr_Upper'][indice-2])and(df['Stop_Atr_Upper'][i-1]<dados['Close'][i]):
                     return True
