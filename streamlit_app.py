640b2ad242d4e0ab7b5340fe0703790d2bfdbfde
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-06-05 11:35:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,46 +21,6 @@ Indicador = st.sidebar.selectbox('Escolha o indicador desejado :',indicadores)
 #Digitar o ativo desejado
 ativo = st.sidebar.text_input("Digite o ativo desejado : ")
 #============================================================================FUNÇAO OPERAÇÕES========================================================================
-def Operacoes(n,df):
-    resultado_compras = []
-    #listas de vendas
-    resultado_vendas = []
-    tam = len(df)
-    #parametro boleano para não comprar duas veses seguidas
-    flag_compra = False
-    #marcação do ultima dia compra
-    dia_ultima_compra = 0
-    for i in range(n,tam):
-        if (podeComprar(i,df)) and (not flag_compra):
-            linha = [df.index[i],df['Close'][i]]
-            resultado_compras.append(linha)
-            flag_compra = True
-            dia_ultima_compra=i
-          #lista de vendas 
-        elif (podeVender(i,df)) and (flag_compra):
-            linha1 = [df.index[i],df['Close'][i]]
-            resultado_vendas.append(linha1)
-            flag_compra = False  
-    resultado_compras = pd.DataFrame(resultado_compras,columns = ["Data","Price_Buy"])
-    resultado_vendas = pd.DataFrame(resultado_vendas,columns = ["Data","Price_Sell"])
-    compras = resultado_compras['Price_Buy'].tolist()
-    vendas = resultado_vendas['Price_Sell'].tolist()
-    lista_compras = []
-    lista_vendas = []
-    for i in range(0,len(df)):
-        if (df['Close'][i] in compras):
-            lista_compras.append(df['Close'][i])
-        else:
-            lista_compras.append(np.NaN)
-    for j in range(0,len(df)):
-        if (df['Close'][j] in vendas):
-            lista_vendas.append(df['Close'][j])
-        else:
-            lista_vendas.append(np.NaN)
-
-    df['Buy'] = lista_compras
-    df['Sell'] = lista_vendas
-    return df
 
 #===================================================================================INICIO APLICATIVO============================================================
 if ativo:
