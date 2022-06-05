d300d2d1fe9ed94e8234600e59c3fb36f61d9f72
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-06-05 20:11:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ col1, col2, col3 = st.beta_columns([1,6,1])
 with col1:
     st.write("")
 with col2:
-    st.image(imagem,with = 500)
+    st.image(imagem,width=1000, height=1500)
 with col3:
     st.write("")
 
