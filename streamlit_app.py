ca85cb562b6a9eb7f4948bf9ac30ee91532fc4f8
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-06-05 19:49:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,11 @@ image = Image.open("TRADE.png")
 st.markdown("<h1 style='text-align: center; color: red;'>APLICATIVO GRÁFICO TRADER OBJETIVO</h1>",unsafe_allow_html=True)
 hide_streamlit_style = """
             <style>
-          
+            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 #Indicadores disponíveis
