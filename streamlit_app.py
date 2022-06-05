5efc3f83c9ebb94ecd878ef892709e16d8eef808
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2022-06-05 20:10:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,11 @@ image = Image.open("TRADE.png")
 col1, col2, col3 = st.beta_columns([1,6,1])
 
 with col1:
-st.write("")
+    st.write("")
 with col2:
-st.image(imagem,with = 500)
+    st.image(imagem,with = 500)
 with col3:
-st.write("")
+    st.write("")
 
 #Iniciando APP
 st.markdown("<h1 style='text-align: center; color: red;'>APLICATIVO GRÁFICO TRADER OBJETIVO</h1>",unsafe_allow_html=True)
